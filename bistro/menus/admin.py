8a09bb3c45f2814c1b24dc8863f529efe4273633
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-11-10 15:33:51 +0000

    added fullmenu, cuisine, and catagory model

---
+++ bistro/menus/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import FullMenu, Cuisine, Category
+
+admin.site.register(FullMenu)
+admin.site.register(Cuisine)
+admin.site.register(Category)
+
+# Register your models here.
---
+++ bistro/menus/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+#  models.IntegerField(default=SpicyLevel.mild, choices = SpicyLevel.choices)
+class FullMenu(models.Model):
+    SPICY_LEVEL_CHOICES = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+    spicy_level = models.IntegerField(choices=SPICY_LEVEL_CHOICES, default=1)
+
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=150)
+    price = models.IntegerField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    cuisine = models.ForeignKey('Cuisine', on_delete=models.CASCADE)
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+class Cuisine(models.Model):
+    name = models.CharField(max_length=30)
+
+# -----------------------------------------------------------------------------------------------------------------
+# using this for a reference to my spicy level subclass
+# class Ticket(models.Model):
+#     class Statuses(models.IntegerChoices):
+#         OPEN = 2, 'Open'
+#         PENDING = 3, 'Pending'
+#         RESOLVED = 4, 'Resolved'
+#         CLOSED = 5, 'Closed'
+#     class Priorities(models.IntegerChoices):
+#         LOW = 1, 'Low'
+#         MEDIUM = 2, 'Medium'
+#         HIGH = 3, 'High'
+#         URGENT = 4, 'Urgent'
+    
+#     priority = models.IntegerField(default=Priorities.LOW, choices = Priorities.choices)
+#     status = models.IntegerField(default=Statuses.OPEN, choices=Statuses.choices)
